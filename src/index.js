796b262941ddfd55fd329a4d9bbb38ce7caa9b64
leandrofn3 <lf1938833@gmail.com>
2023-05-07 19:48:15 -0300

    criação do cadrastro jundo com validações

--- src/index.js
+++ src/index.js
@@ -1,19 +1,68 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
+import session from 'express-session';
 
 const app = express();
 
 app.use(express.json());
 
-function middleware(request, response, next){
-    
+function middlewareId(request, response, next) {
+    const id = Number(request.params.id);
+    const index = recados.findIndex(recadoId => recadoId.id === id);
+    if (index == -1) {
+        return response.status(400).json("Por favor, insira um id valido!");
+    } else {
+        next()
+    }
 };
 
 app.get("/", (request, response) => {
-    return response.json("ok")
+    return response.json("Bem vindo(a) a API!")
 });
+
+//sinup
+
+let users = [];
+
+app.get("/signup", (request, response) => {
+    response.render("signup");
+})
+app.post("/signup", middlewareValidationSignup, (request, response) => {
+    const user = request.body;
+    users.push({
+        id: Math.floor(Math.random() * 123456),
+        name: user.nome,
+        email: user.email,
+        password: user.password,
+        recados: recados = []
+    });
+    response.status(201).json("Conta criada com sucesso");
+});
+
+app.get("/signup", (request, response) => {
+    response.status(200).json();
+})
+
+function middlewareValidationSignup(request, response, next) {
+    const sameEmail = users.some((emailSame) => {
+        return emailSame.email === request.body.email
+    });
+    if (sameEmail) {
+        return response.status(422).json("Email já existe, tente outro!")
+    } else if (!request.body.name) {
+        return response.status(422).json("O nome é obrigatório! ")
+    } else if (!request.body.email) {
+        return response.status(422).json("O email é obrigatório! ")
+    } else if (!request.body.password) {
+        return response.status(422).json("A senha é obrigatória! ")
+    } else {
+        next()
+    };
+};
 
 //CRUD RECADOS
 //CREATE
+
 let recados = [];
 
 app.post("/recados", (request, response) => {
@@ -28,12 +77,14 @@ app.post("/recados", (request, response) => {
 });
 
 //READE-ler.
+
 app.get("/recados", (request, response) => {
     response.status(200).json(recados);
 });
 
 //READE - ler um recado só.
-app.get("/recados/:id", (request, response) => {
+
+app.get("/recados/:id", middlewareId, (request, response) => {
     const id = Number(request.params.id);
     const recado = recados.find(recadoId => recadoId.id === id);
     response.json(recado);
@@ -41,7 +92,8 @@ app.get("/recados/:id", (request, response) => {
 });
 
 //UPDATE - alterar um recado.
-app.put("/recados/:id", (request, response) => {
+
+app.put("/recados/:id", middlewareId, (request, response) => {
     const recado = request.body;
     const id = Number(request.params.id);
     const indexRecado = recados.findIndex(recadoId => recadoId.id === id);
@@ -55,7 +107,7 @@ app.put("/recados/:id", (request, response) => {
 
 //DELETE - deletar um recado
 
-app.delete("/recados/:id", (request, response)=> {
+app.delete("/recados/:id", middlewareId, (request, response) => {
     const id = Number(request.params.id);
     const indexRecado = recados.findIndex(recadoId => recadoId.id === id);
     recados.splice(indexRecado, 1);
