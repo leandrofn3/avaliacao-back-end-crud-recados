c1fdb66e71965d90284961ad097683612c65a077
leandrofn3 <lf1938833@gmail.com>
2023-05-12 22:34:09 -0300

    validando para que cada recado seja de um unico usuario e exclusão de algumas biblilhotecas desnecessarias

--- src/index.js
+++ src/index.js
@@ -34,15 +34,16 @@ app.post("/signup", middlewareValidationSignup, (request, response) => {
                 name: user.name,
                 email: user.email,
                 password: hash,
-                recados: recados = []
+                recados: []
             });
         } else {
             return response.status(400).json(`Houve um erro: ${err}`)
         }
-    }); console.log(users)
+    }); 
 
     response.status(201).json("Conta criada com sucesso");
 });
+console.log(users)
 
 app.get("/signup", (request, response) => {
     response.status(200).json(users);
@@ -91,58 +92,74 @@ app.post('/login', async (request, response) => {
     if (!checkPassword) {
         return response.status(422).json({ message: 'Senha inválida!' });
     }
-    
+    return response.status(200).json("usuário logado!");
 });
 //CRUD RECADOS
 //CREATE
 
-let recados = [];
-
-app.post("/recados", (request, response) => {
+app.post("/recados/create/:id", (request, response) => {
+    const id = Number(request.params.id);
     const recado = request.body;
-    recados.push({
+    const validateID = users.findIndex(userID => userID.id === id);
+
+    if(validateID === -1){
+        return response.status(404).json("usuario não encontrado!");
+    }
+
+    console.log(users);
+    // trocar um push de objeto por um array
+    const newRecado = {
         id: Math.floor(Math.random() * 123456),
         titulo: recado.titulo,
         descricao: recado.descricao
-    })
-    console.log(recados)
-    response.status(201).json();
+    };
+    users[validateID].recados.push(newRecado);
+    response.status(201).json(newRecado);
 });
 
 //READE-ler.
 
-// app.get("/recados", (request, response) => {
-//     response.status(200).json(recados);
-// });
+app.get("/recados/reade", (request, response) => {
+    response.status(200).json(users[4]);
+});
 
 //READE - ler um recado só.
 
-app.get("/recados/:id", middlewareId, (request, response) => {
+app.get("/recados/readeID/:id", middlewareId, (request, response) => {
     const id = Number(request.params.id);
-    const recado = recados.find(recadoId => recadoId.id === id);
+    const recado = users.find(recadoId => recadoId.id === id);
     response.json(recado);
     //console.log(recado)
 });
 
 //UPDATE - alterar um recado.
 
-app.put("/recados/:id", middlewareId, (request, response) => {
+app.put("/recados/update/:id/:idUser", middlewareId, (request, response) => {
     const recado = request.body;
+
+    const idUser = Number(request.params.idUser);
+    const validateIDUser= users.findIndex(user => user.id === id);
+
+    if(validateIDUser === -1){
+        return response.status(404).json("usuario não encontrado!");
+    };
+    
     const id = Number(request.params.id);
-    const indexRecado = recados.findIndex(recadoId => recadoId.id === id);
-    recados[indexRecado] = {
+    const indexRecado = users[validateIDUser].findIndex(recadoId => recadoId.id === id);
+
+    users[validateIDUser].recados[indexRecado] = {
         id: id,
         titulo: recado.titulo,
         descricao: recado.descricao
     }
-    response.status(201).json();
+    response.status(200).json();
 });
 
 //DELETE - deletar um recado
 
-app.delete("/recados/:id", middlewareId, (request, response) => {
+app.delete("/recados/delete/:id", middlewareId, (request, response) => {
     const id = Number(request.params.id);
-    const indexRecado = recados.findIndex(recadoId => recadoId.id === id);
+    const indexRecado = users.findIndex(recadoId => recadoId.id === id);
     recados.splice(indexRecado, 1);
     response.status(200).json();
 });
